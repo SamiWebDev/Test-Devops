3a1bb90bad335da92ef4f76d2c1e0d6b65f5daa9
samito4 <sami.mimun1@gmail.com>
2024-09-26 19:36:21 +0200

    hola

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ app.use(express.json());
 
 // Ruta básica
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello World! From Sami" });
+  res.status(200).json({ message: "Hello World! From Sami Mimun" });
 });
 
 // Ruta de ejemplo para pruebas
