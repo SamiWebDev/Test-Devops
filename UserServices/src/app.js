91dfee3980169ae7d3188ac39ca4248eb8bf3db1
samito4 <sami.mimun1@gmail.com>
2024-09-26 19:22:44 +0200

    arereglos de fly

--- UserServices/src/app.js
+++ UserServices/src/app.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 app.use(express.json());
 
 // Ruta básica
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello World!' });
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World! From Sami" });
 });
 
 // Ruta de ejemplo para pruebas
-app.post('/echo', (req, res) => {
-    const { body } = req;
-    res.status(200).json(body);
+app.post("/echo", (req, res) => {
+  const { body } = req;
+  res.status(200).json(body);
 });
 
 module.exports = app;
